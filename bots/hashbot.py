e6e2c0dcc3151d013cf5324cce0d3f9d8f894206
flamebarke <39644720+flamebarke@users.noreply.github.com>
2022-09-05 01:18:02 +0800

    Update hashbot.py

--- bots/hashbot.py
+++ bots/hashbot.py
@@ -6,7 +6,7 @@ __version__ = "1.0.0"
 __description__ = """
                 This bot will return the hashes obtained using discohash
                 for a specified number of access points as a txt file.
-                Message the bot with !dumphash [NUMBER OF APS].
+                Message the bot with !dumphash [NUMBER OF APs].
                 """
 
 import os
